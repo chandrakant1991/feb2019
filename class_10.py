545b5808506f316397f86bd3e4151c6907605d4e
chandrakant Patil <chandrakant.patil66@gmail.com>
2019-03-03 12:42:57 +0530

    class09

---
+++ class_10.py
@@ -0,0 +1,31 @@
+#
+# 1) Program to accept the strings which contains all vowels
+# 2) Palindrome numbers in a list
+# 3) Number to words like 121 converts one two one or one hundred twenty one
+
+
+
+
+
+# list1 = [121, 100, 201, 252, 656, 589, 248, 494, 10]
+# # list2 = list1
+#
+# for i in list1:
+#     n = i
+#     rev = 0
+#     while i>0:
+#         dig =i%10
+#         rev= rev*10+dig
+#         i=i//10
+#     if(n==rev):
+#         print(rev)
+#
+
+
+
+
+# string = input('Enter The String==>')
+# string.split()
+#
+
+
